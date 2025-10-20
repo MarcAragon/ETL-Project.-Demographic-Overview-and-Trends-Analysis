4c20540f8ba41804dbabaf44c4a05ffb21cd4056
MarcAragon <162532559+MarcAragon@users.noreply.github.com>
2025-10-20 14:07:25 -0500

    Airflow Migration

--- dags/MainDag.py
+++ dags/MainDag.py
@@ -2,6 +2,8 @@ from airflow.decorators import dag
 from datetime import datetime
 from tasks.Extract import Extract
 from tasks.Transform import Transform
+from tasks.CreateDatabase import Create_Tables
+from tasks.Load import DB_Data_Upload
 
 @dag(
     dag_id='Proyecto2Prueba',
@@ -15,6 +17,8 @@ def Pipeline_Prueba():
 
     Dfs = Extract()
     DfsClean = Transform(Dfs)
+    Create_Tables()
+    DB_Data_Upload(DfsClean)
 
 
 Pipeline_Prueba()
